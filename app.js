08094e2a835e0bc052ef540eb158eabda03b5266
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-22 11:43:29 +0600

    design of modal completed

---
+++ app.js
@@ -0,0 +1,11 @@
+const selectedSeatEl = document.getElementById("selected-seat");
+
+// handle selection seat
+function handSelectSeat(event) {
+  selectedSeatEl.innerHTML = 
+  `<li class="flex justify-between">
+        <span>${even.innerte}</span>
+        <span></span>
+        <span></span>
+    </li>`;
+}
